47e3e065559970bfbd81a0586ed32b64dc60fe36
Michał 'Wolvverine' Panasiewicz <wolvverine@pld-linux.org>
2005-12-12 01:30:49 +0000

    - new version
    Changed files:
        wpkg-start.bat -> 1.3

--- wpkg-start.bat
+++ wpkg-start.bat
@@ -1,1 +1,13 @@
-cscript \\serwer\wpkg\wpkg.js /synchronize /quiet
+set WPKGROOT=\\server\wpkg
+set PACKAGES=%WPKGROOT%\packages
+cscript %WPKGROOT%\wpkg\wpkg.js /synchronize /nonotify /quiet
+
+REM This one is a recommended way of starting WPKG
+REM \\server\wpkg\wpkg.js /synchronize /quiet
+REM 
+REM Use this one if you don't want the notify window
+REM \\server\wpkg\wpkg.js /synchronize /quiet /nonotify
+REM 
+REM Use this one when logged in as Administrator to debug the problems
+REM \\server\wpkg\wpkg.js /synchronize /debug
+
